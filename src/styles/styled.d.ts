2e723c0853f08f489a6650d0a00ed3b8f98ede4b
Filipe Colaquecez <filipepcolaquecez@gmail.com>
2022-04-09 22:34:59 -0300

    feat: styledcomponents setup

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,11 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    WHITE: string;
+    PRIMARY: string;
+    HINT: string;
+    TEXT: string;
+    BACKGROUND: string;
+  }
+}
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+const colors = {
+  WHITE: 'rgba(255,255,255,01)',
+  PRIMARY: '#c72a2a',
+  HINT: 'rgba(0,0,0,0.35)',
+  TEXT: 'rgba(0,0,0,0.85)',
+  BACKGROUND: 'rgba(255,255,255,01)'
+};
+
+export { colors };
